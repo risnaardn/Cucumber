7f028fc98e3b4520169f69d38002f0fa411a3ff0
Risna Putri Ramadhani <risnaardn@gmail.com>
2022-10-28 20:46:20 +0700

    first commit

--- src/main/java/com/juaracoding/cucumber/drivers/strategies/DriverStrategyImplementer.java
+++ src/main/java/com/juaracoding/cucumber/drivers/strategies/DriverStrategyImplementer.java
@@ -2,11 +2,16 @@ package com.juaracoding.cucumber.drivers.strategies;
 
 import com.juaracoding.cucumber.utils.Constants;
 
+import static com.juaracoding.cucumber.utils.Constants.CHROMEHEADLESS;
+
 public class DriverStrategyImplementer {
     public static DriverStrategy chooseStrategy(String strategy) {
         switch (strategy) {
             case Constants.CHROME:
                 return new Chrome();
+
+            case CHROMEHEADLESS:
+                return (DriverStrategy) new ChromeHeadless();
 
             case Constants.FIREFOX:
                 return new Firefox();
